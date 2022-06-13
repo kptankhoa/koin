00221d8e8b4d206b193221d5a9f9a6d6c284e2f8
Khoa Phan <kptankhoa@gmail.com>
2022-06-13 09:53:56 +0700

    update chain api test

--- server/__test__/chain_api.test.js
+++ server/__test__/chain_api.test.js
@@ -1,30 +1,30 @@
 const createServer = require('../src/utils/server.util');
-const request = require('supertest');
+const supertest = require('supertest');
 const _ = require('lodash');
 const { genesisBlock } = require('../src/constants/blockchain.const');
 const { isValidChain } = require('../src/utils/chain.util');
 
 const app = createServer();
+const request = supertest(app);
 
 describe('test chain apis', () => {
-  describe('get chain api', () => {
-
+  describe('GET /chain', () => {
     it('should be 200', async () => {
-      await request(app).get('/chain').expect(200);
+      await request.get('/chain').expect(200);
     });
 
     it('should be an array body', async () => {
-      const { body } = await request(app).get('/chain');
+      const { body } = await request.get('/chain');
       expect(_.isArray(body)).toBe(true);
     });
 
     it('has valid genesis block', async () => {
-      const { body } = await request(app).get('/chain');
+      const { body } = await request.get('/chain');
       expect(body[0]).toEqual(genesisBlock);
     });
 
     it('should have valid chain', async () => {
-      const { body } = await request(app).get('/chain');
+      const { body } = await request.get('/chain');
       expect(isValidChain(body));
     });
 
